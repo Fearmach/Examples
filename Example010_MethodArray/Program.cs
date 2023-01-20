0d9fee7976c003f9f75c0016f1b90202e9b5cd27
Evgeny <beloysov_jenya@inbox.ru>
2023-01-20 22:50:30 +0300

    add example10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// See ДЕЛАЕМ МАССИВ С НЕ ОГРАНИЧЕННЫМ ЧИСЛОМ ЦИФР (ПО ЗАДАННОМУ ЧИСЛУ НАХОДИМ ПОД КАКИМ НОМЕРОМ ЭТО ЧИСЛО В МАССИВЕ)
+
+int[] array = { 11, 2, 33, 43, 84, 5, 67, 7, 84 };
+
+int n = array.Length;
+int find = 84;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
