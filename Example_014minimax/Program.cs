1ab117c8f7825ef05ef9368127e5e7bcea0da803
Evgeny <beloysov_jenya@inbox.ru>
2023-01-27 16:13:33 +0300

    add example 14

---
+++ Example_014minimax/Program.cs
@@ -0,0 +1,34 @@
+﻿// Установить минимум и менять мин числа с мах по порядку
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.WriteLine($"{array[i]}");
+    }
+    System.Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minpos = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minpos]) minpos = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minpos];
+        array[minpos] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
