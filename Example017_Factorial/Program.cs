4e87bb933acfd02542b7e4ced383681179cf76f6
Evgeny <beloysov_jenya@inbox.ru>
2023-02-02 23:00:33 +0300

    Example17

---
+++ Example017_Factorial/Program.cs
@@ -0,0 +1,13 @@
+﻿// Рекурсия факториала
+
+double Factorial (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    System.Console.WriteLine($"{i}! = {Factorial(i)}"); // 1*2*3 = 6
+}
