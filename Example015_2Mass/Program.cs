c03fab8fda2a55c56e6308a37a9ec2db509c66f9
Evgeny <beloysov_jenya@inbox.ru>
2023-02-02 22:03:14 +0300

    Example15

---
+++ Example015_2Mass/Program.cs
@@ -0,0 +1,39 @@
+﻿// string[,] table = new string[2,5];
+// table[1, 2] = "Слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int colums = 0; colums < 5; colums++)
+//     {
+//         System.Console.WriteLine($"-{table[rows, colums]}-");
+//     }
+// }
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+System.Console.WriteLine();
+PrintArray(matrix);
