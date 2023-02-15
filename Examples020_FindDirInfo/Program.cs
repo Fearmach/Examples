e0bda4e8603814464e372cb5fbbc528d5edc4975
Evgeny <beloysov_jenya@inbox.ru>
2023-02-16 00:26:34 +0300

    add tasks 19, 20, 21, 22, 23

---
+++ Examples020_FindDirInfo/Program.cs
@@ -0,0 +1,36 @@
+﻿// Поиск различной информации в папках(дериктории)
+
+string path = "C:\Users\beloy\OneDrive\Рабочий стол\Учеба\С# Семинары, лекции 3.0\Лекции example\Example_001Helloworld";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
+
+// РЕКУРСИЯ
+
+void CatalogInfo[] (string path, string indent = '')
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalog = catalog.GetDerectories();
+    for (int i = 0; i < catalog.Length; i++)
+    {
+        System.Console.WriteLine($"{indent}{catalog[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        System.Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = @"C:\Users\beloy\OneDrive\Рабочий стол\Учеба\С# Семинары, лекции 3.0\Лекции example\Example_001Helloworld";
+CatalogInfo(path);
+
---
+++ Examples023_СчитаемКоличествоИтераций/Program.cs
@@ -0,0 +1,54 @@
+﻿// СЧИТАЕМ КОЛИЧЕСТВО ДЕЙСТВИЙ(ИТЕРАЦИЙ) В ОБЫЧНОМ МЕТОДЕ И РЕКУРСИИ
+
+decimal fRec = 0;
+decimal fIte = 0;
+
+decimal FibonacciRecursion(int n)
+{
+    fRec++;
+    return n == 0 || n == 1 ? 1 : FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2); 
+}
+
+decimal FibonacciIteration(int n)
+{
+    fIte++;
+    decimal result = 1;
+    decimal f0 = 1;
+    decimal f1 = 1;
+    for (int i = 0; i < n; i++)
+    {
+        result = f0 + f1;
+
+        f0 = f1;
+        f1 = result;
+        fIte++;
+    }
+    return result;
+}
+
+//# 1 2 3 4 5 6
+//V 1 1 2 3 5 8
+System.Console.ReadLine();
+
+DateTime dt = DateTime.Now;
+for (int n = 1; n < 20; n++)
+{
+    System.Console.WriteLine($"FibonacciIteration({n}) = {FibonacciIteration(n)} fIte = {fIte.ToString("### ### ###"), - 15}");
+    fIte = 0;
+}
+System.Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+System.Console.WriteLine();
+System.Console.ReadLine();
+
+for (int n = 1; n < 20; n++)
+{
+    System.Console.WriteLine($"FibonacciRecursion({n}) = {FibonacciRecursion(n)} fIte = {fRec.ToString("### ### ###"), - 15}");
+    fRec = 0;
+}
+System.Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+
+
+
+
